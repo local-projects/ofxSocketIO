f30c26637ac860bc5b73235e6e07163adb786a6e
Mathieu Skull <mathieu@themojoes.eu>
2020-01-02 11:33:17 +0200

    Replace socket.io source with latest ones.

--- libs/socket.io-client/include/sio_client.h
+++ libs/socket.io-client/include/sio_client.h
@@ -58,6 +58,9 @@ namespace sio
 
         void connect(const std::string& uri, const std::map<std::string,std::string>& query);
 
+        void connect(const std::string& uri, const std::map<std::string,std::string>& query,
+                     const std::map<std::string,std::string>& http_extra_headers);
+
         void set_reconnect_attempts(int attempts);
 
         void set_reconnect_delay(unsigned millis);
@@ -77,8 +80,8 @@ namespace sio
         
     private:
         //disable copy constructor and assign operator.
-        client(client const& cl){}
-        void operator=(client const& cl){}
+        client(client const&){}
+        void operator=(client const&){}
         
         client_impl* m_impl;
     };
